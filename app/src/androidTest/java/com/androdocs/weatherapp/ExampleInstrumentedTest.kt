d96eff95bcabf0c925170bed2a320989aa670745
unknown <alexander.dragomir@gmail.com>
2020-05-24 16:27:06 +0300

    AppCommit

---
+++ app/src/androidTest/java/com/androdocs/weatherapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package com.androdocs.weatherapp
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.androdocs.weatherapp", appContext.packageName)
+    }
+}
